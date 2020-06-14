9db33426c29a8d71b7316cb3539608c830db7263
ahewitt-glasswall <59472395+ahewitt-glasswall@users.noreply.github.com>
2020-06-14 04:54:06 +0100

    deleted gh-pages folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,46 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     resolve: {
         modules: [
-            path.resolve('./src'),
-            path.resolve('./node_modules')
+            path.resolve("./src"),
+            path.resolve("./node_modules")
         ],
         extensions: [
-            '.js', '.jsx', '.css', '.sass'
+            ".js", ".jsx", ".css", ".sass"
         ]
     },
-    entry: ['@babel/polyfill', path.resolve(__dirname, 'src/index.jsx')],
+    entry: ["@babel/polyfill", path.resolve(__dirname, "src/index.jsx")],
     output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'bundle.js',
-        publicPath: '/'
+        path: path.resolve(__dirname, "./dist"),
+        filename: "bundle.js",
+        publicPath: "/"
     },
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
-                include: path.resolve(__dirname, 'src'),
-                use: ['babel-loader']
+                include: path.resolve(__dirname, "src"),
+                use: ["babel-loader"]
             },
             {
                 test: /\.(css|scss)$/,
                 use: [
                     {
-                        loader: 'style-loader'
+                        loader: "style-loader"
                     },                    
                     {
-                        loader: 'css-loader',
+                        loader: "css-loader",
                         options: {
                             importLoaders: 1,
                             modules: {
-                                localIdentName: '[name]_[local]_[hash:base64:5]'
+                                localIdentName: "[name]_[local]_[hash:base64:5]"
                             }
                         }
                     },
                     {
-                        loader: 'sass-loader'
+                        loader: "sass-loader"
                     },
                 ],
                 include: /\.module\.(css|scss)$/
@@ -48,28 +48,28 @@ module.exports = {
             {
                 test: /\.(css|scss)$/,
                 use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
                 ],
                 exclude: /\.module\.(css|scss)$/
             }
         ]
     },
     devServer: {
-        contentBase: path.resolve(__dirname, 'dist'),
+        contentBase: path.resolve(__dirname, "dist"),
         port: 3000,
         open: true,
         historyApiFallback: {
-            index: '/'
+            index: "/"
         },
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'public/index.html',
-            favicon: './public/favicon.ico',
+            template: "public/index.html",
+            favicon: "./public/favicon.ico",
             inject: false
         })
     ],
-    devtool: 'inline-source-map'
+    devtool: "inline-source-map"
 };
