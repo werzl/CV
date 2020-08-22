d0e63deb5c3e8a6a66a36303a1a5f2ccf45a9ba7
werzl <adam@houseofhewitt.co.uk>
2020-08-22 19:17:51 +0100

    education section now fetching content from github

--- gh-pages/src/components/Education/Education.jsx
+++ gh-pages/src/components/Education/Education.jsx
@@ -1,48 +1,41 @@
-import React from "react";
+import React, {useState, useCallback, useEffect} from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import ListGroup from "react-bootstrap/ListGroup";
 import ListGroupItem from "react-bootstrap/ListGroupItem";
 
-import EssexUniversityLogo from "./essex-university-logo.gif";
-import HaveringCollegeLogo from "./havering-college-logo.png";
-import ReddenCourtLogo from "./redden-court-logo.png";
-
 import "./education.scss";
 
-const schools = [
-    {
-        img: EssexUniversityLogo,
-        title: "University of Essex",
-        dateFrom: "2015",
-        dateTo: "2018",
-        details: [
-            { key: "(BSc) Bachelor's Degree in Computer Science", value: "" },
-            { key: "Grade", value: "2:1" },
-            { key: "Modules", value: "Information Retrieval, Natural Language Engineering, Object-Oriented Programming, Web Development, Data-Structures & Algorithms, Software Engineering and Computer Vision." }
-        ]
-    },
-    {
-        img: HaveringCollegeLogo,
-        title: "Havering College",
-        dateFrom: "2012",
-        dateTo: "2014",
-        details: [
-            { key: "A Level", value: "BTEC Level 3 Extended Diploma - Distinction/Distinction/Merit (A, A, C at A-Level)" }
-        ]
-    },
-    {
-        img: ReddenCourtLogo,
-        title: "Redden Court School",
-        dateFrom: "2007",
-        dateTo: "2012",
-        details: [
-            { key: "GCSE", value: "IT: B, Mathematics: B, English: C, History: B, Science: C, Geography: C" }
-        ]
-    }
-];
-
 const Education = () => {
+    const [schools, setSchools] = useState([]);
+
+    const schoolsUrl = "https://raw.githubusercontent.com/werzl/CV/master/content/education/schools.json";
+
+    const fetchSchools = useCallback(async (url) => {
+        try {
+            const response = await fetch(url, {
+                method: "GET"
+            });
+
+            let responseString = await response.json();
+
+            if (response.ok) {
+                setSchools(responseString);
+            }
+            else {
+                console.error(`Error ${response.status}. ${responseString}`);
+            }
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }, [setSchools]);
+
+    useEffect(() => {
+        fetchSchools(schoolsUrl);
+    }, []);
+
+
     return (
         <section className="page-section" id="education">
             <div id="education-scrollspy"></div>
@@ -64,7 +57,7 @@ const Education = () => {
                                 </Row>
                                 <Row>
                                     <Col md="3" className="text-center">
-                                        <img src={school.img} />
+                                        <img src={school.img} alt={school.title}/>
                                     </Col>
                                     <Col>
                                         <p>
