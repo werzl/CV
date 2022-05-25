32b872e7e4f9de317656ceea19299a1bfac693d8
werzl <adam@houseofhewitt.co.uk>
2022-05-25 11:21:49 +0100

    only show navbar on small screens

--- gh-pages/src/components/CvNavbar/CvNavbar.jsx
+++ gh-pages/src/components/CvNavbar/CvNavbar.jsx
@@ -6,18 +6,13 @@ import Nav from "react-bootstrap/Nav";
 
 const CvNavbar = () => {
     return (
-        <Navbar collapseOnSelect fixed="top" expand="lg" bg="secondary" variant="dark" id="mainNav" className="text-uppercase">
+        <Navbar collapseOnSelect fixed="top" expand="lg" bg="secondary" variant="dark" id="mainNav" className="text-uppercase d-md-none">
             <div className="container">
                 <Navbar.Brand href="#">AH</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
                 <Navbar.Collapse id="navbarResponsive">
                     <Nav className="ml-auto">
-                        <Nav.Item className="text-center mr-2">
-                            <a href="https://github.com/werzl/CV" target="_blank" rel="noreferrer">
-                                <svg className="github-icon ml-5" style={{ width: "3rem", height: "3rem" }}></svg>
-                            </a>
-                        </Nav.Item>
                         <Nav.Item>
                             <Scrollspy items={["skills-scrollspy"]} currentClassName="scrolled" className="p-0 m-0" offset={100}>
                                 <Nav.Link href="#skills">Skills</Nav.Link>
