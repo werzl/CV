86388843181daf05dcec00a1b4204111d2012809
werzl <adam@houseofhewitt.co.uk>
2022-05-25 10:56:37 +0100

    update colours on nav and masthead

--- gh-pages/src/components/Masthead/Masthead.jsx
+++ gh-pages/src/components/Masthead/Masthead.jsx
@@ -13,12 +13,8 @@ const socialAccounts = [
 
 const Masthead = () => {
     return (
-        <header className="masthead text-center bg-primary text-white">
+        <header className="masthead text-center text-white">
             <div className="container align-items-center">
-                <img
-                    src="https://avatars0.githubusercontent.com/u/16911902?s=460&u=9a6c95dd36efb2eb35af8638962aa61851f01e62&v=4"
-                    className="masthead-avatar mb-5" />
-
                 <h1 className="masthead-heading text-uppercase mb-0">Adam Hewitt</h1>
 
                 <p className="masthead-subheading">
